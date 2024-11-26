35eed2888978248d1c88f2c4ca3c46bac0d5e317
Ichurri <santiagoiturrivargas04@gmail.com>
2024-11-25 20:52:37 -0400

    refactor: order functions in index.js

--- index.js
+++ index.js
@@ -34,12 +34,184 @@ document.getElementById("login-form").addEventListener("submit", (event) => {
   }
 });
 
-// Actualizar saldo total y guardar en localStorage
-const actualizarSaldoTotal = () => {
-  const saldoTotal = transaccion.calcularSaldoTotal();
-  document.getElementById("saldo-total").textContent = `$${saldoTotal}`;
-  guardarEnLocalStorage("saldoTotal", saldoTotal);
+document.getElementById("form-categorias").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const nombreCategoria = document.getElementById("nombre-categoria").value;
+
+  if (nombreCategoria.trim() === "") {
+    alert("Por favor, ingresa un nombre válido para la categoría.");
+    return;
+  }
+
+  categorias.crearCategoria(nombreCategoria);
+  const categoriasGuardadas = obtenerDeLocalStorage("categorias") || [];
+  categoriasGuardadas.push(nombreCategoria);
+  guardarEnLocalStorage("categorias", categoriasGuardadas);
+
+  mostrarCategorias();
+
+  document.getElementById("form-categorias").reset();
+});
+
+document.getElementById("selector-categoria").addEventListener("change", (event) => {
+  const categoriaSeleccionada = event.target.value;
+  const elementoCategoriaSeleccionada = document.getElementById(
+    "categoria-seleccionada"
+  );
+
+  if (categoriaSeleccionada) {
+    elementoCategoriaSeleccionada.textContent = `Categoría seleccionada: ${categoriaSeleccionada}`;
+  } else {
+    elementoCategoriaSeleccionada.textContent = "";
+  }
+});
+
+const mostrarCategorias = () => {
+  const categoriasGuardadas = obtenerDeLocalStorage("categorias") || [];
+  const listaCategorias = document.getElementById("lista-categorias");
+  const selectorCategoria = document.getElementById("selector-categoria");
+
+  listaCategorias.innerHTML = "";
+  selectorCategoria.innerHTML =
+    '<option value="">Selecciona una categoría</option>';
+
+  categoriasGuardadas.forEach((categoria) => {
+    const elementoCategoria = document.createElement("li");
+    elementoCategoria.textContent = categoria;
+    listaCategorias.appendChild(elementoCategoria);
+
+    // Añadir categoría al selector
+    const opcion = document.createElement("option");
+    opcion.value = categoria;
+    opcion.textContent = categoria;
+    selectorCategoria.appendChild(opcion);
+  });
 };
+
+document.addEventListener("DOMContentLoaded", mostrarCategorias);
+
+// Gestión de Ingresos
+// Variables de Referencia para Ingresos
+const formIngreso = document.getElementById("form-ingreso");
+const botonAgregarIngreso = document.getElementById("btn-agregar-ingreso");
+const botonActualizarIngreso = document.getElementById("btn-actualizar-ingreso");
+let indexEnEdicionIngreso = null;
+
+
+// Registrar Ingreso
+botonAgregarIngreso.addEventListener("click", (e) => {
+  e.preventDefault();
+  const cantidad = parseFloat(
+    document.getElementById("cantidad-ingreso").value
+  );
+  const descripcion = document.getElementById("descripcion-ingreso").value;
+  const fecha = new Date().toLocaleDateString();
+
+  if (isNaN(cantidad) || descripcion.trim() === "") {
+    alert("Por favor, completa los campos correctamente.");
+    return;
+  }
+
+  transaccion.registrarIngreso(cantidad, descripcion, fecha);
+  guardarEnLocalStorage("ingresos", transaccion.obtenerHistorialIngresos());
+  mostrarHistorialIngresos();
+  actualizarSaldoTotal();
+  formIngreso.reset();
+});
+
+
+// Editar y Eliminar Ingresos
+document.getElementById("cuerpo-historial-ingresos").addEventListener("click", (event) => {
+    const boton = event.target;
+
+    // Editar ingreso
+    if (boton.classList.contains("editar-btn-ingreso")) {
+      const index = boton.dataset.index;
+      const ingreso = transaccion.obtenerHistorialIngresos()[index];
+
+      document.getElementById("cantidad-ingreso").value = ingreso.cantidad;
+      document.getElementById("descripcion-ingreso").value = ingreso.descripcion;
+
+      indexEnEdicionIngreso = index;
+      botonAgregarIngreso.style.display = "none";
+      botonActualizarIngreso.style.display = "block";
+    }
+    // Eliminar Ingreso
+    if (boton.classList.contains("eliminar-btn-ingreso")) {
+      const index = boton.dataset.index;
+      transaccion.eliminarIngreso(index);
+      mostrarHistorialIngresos();
+      actualizarSaldoTotal();
+    }
+  });
+
+// Actualizar Ingreso Existente
+botonActualizarIngreso.addEventListener("click", () => {
+  const cantidad = parseFloat(
+    document.getElementById("cantidad-ingreso").value
+  );
+  const descripcion = document.getElementById("descripcion-ingreso").value;
+  const fecha = new Date().toLocaleDateString();
+
+  if (isNaN(cantidad) || descripcion.trim() === "") {
+    alert("Por favor, completa los campos correctamente.");
+    return;
+  }
+  transaccion.editarIngreso(
+    indexEnEdicionIngreso,
+    cantidad,
+    descripcion,
+    fecha
+  );
+  // Restablecer el formulario y botones
+  formIngreso.reset();
+  botonAgregarIngreso.style.display = "block";
+  botonActualizarIngreso.style.display = "none";
+  guardarEnLocalStorage("ingresos", transaccion.obtenerHistorialIngresos());
+  mostrarHistorialIngresos();
+  actualizarSaldoTotal();
+});
+
+// Guardar un ingreso en localStorage
+document.getElementById("form-ingreso").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const cantidad = parseFloat(
+    document.getElementById("cantidad-ingreso").value
+  );
+  const descripcion = document.getElementById("descripcion-ingreso").value;
+  const fecha = new Date().toLocaleDateString();
+
+  transaccion.registrarIngreso(cantidad, descripcion, fecha);
+  guardarEnLocalStorage("ingresos", transaccion.obtenerHistorialIngresos());
+  mostrarHistorialIngresos();
+  actualizarSaldoTotal();
+});
+
+
+// Mostrar Historial de Ingresos
+const mostrarHistorialIngresos = () => {
+  const cuerpoHistorialIngresos = document.getElementById
+  ("cuerpo-historial-ingresos");
+  cuerpoHistorialIngresos.innerHTML = "";
+  const ingresos = transaccion.obtenerHistorialIngresos();
+
+  ingresos.forEach((ingreso, index) => {
+    const fila = document.createElement("tr");
+    fila.innerHTML = `
+      <td>${ingreso.cantidad}</td>
+      <td>${ingreso.descripcion}</td>
+      <td>${ingreso.fecha}</td>
+      <td>
+        <button class="editar-btn-ingreso" data-index="${index}">Editar</button>
+        <button class="eliminar-btn-ingreso" data-index="${index}">Eliminar</button>
+      </td>
+    `;
+    cuerpoHistorialIngresos.appendChild(fila);
+  });
+}
+
 
 // Referencias a botones y elementos del DOM
 const botonAgregar = document.getElementById("btn-agregar");
@@ -47,27 +219,42 @@ const botonActualizar = document.getElementById("btn-actualizar");
 const formGasto = document.getElementById("form-gasto");
 // Variable para almacenar el índice del gasto en edición
 let indexEnEdicion = null;
-// Función para mostrar el historial de gastos
-function mostrarHistorialGastos() {
-  const cuerpoHistorial = document.getElementById("cuerpo-historial-gastos");
-  cuerpoHistorial.innerHTML = "";
-  const gastos = transaccion.obtenerHistorialGastos();
 
-  gastos.forEach((gasto, index) => {
-    const fila = document.createElement("tr");
-    fila.innerHTML = `
-      <td>${gasto.cantidad}</td>
-      <td>${gasto.descripcion}</td>
-      <td>${gasto.categoria}</td>
-      <td>${gasto.fecha}</td>
-      <td>
-        <button class="editar-btn" data-index="${index}">Editar</button>
-        <button class="eliminar-btn" data-index="${index}">Eliminar</button>
-      </td>
-    `;
-    cuerpoHistorial.appendChild(fila);
-  });
-}
+// Registrar nuevo gasto
+botonAgregar.addEventListener("click", (e) => {
+  e.preventDefault();
+  const cantidad = parseFloat(
+    document.getElementById("cantidad-gasto").value
+  );
+  const descripcion = document.getElementById("descripcion-gasto").value;
+  const categoria = document.getElementById("selector-categoria").value;
+  const fecha = new Date().toLocaleDateString();
+
+  if (isNaN(cantidad) || descripcion.trim() === "") {
+    alert("Por favor, completa todos los campos correctamente.");
+    return;
+  }
+
+  if (categoria) {
+    transaccion.registrarGasto(
+      cantidad,
+      descripcion,
+      fecha,
+      categoria
+      
+    );
+  } else {
+    transaccion.registrarGasto(cantidad, descripcion, fecha,'sin categoria');
+  }
+
+  document.getElementById("categoria-seleccionada").textContent = "";
+  guardarEnLocalStorage("gastos", transaccion.obtenerHistorialGastos());
+  mostrarHistorialGastos();
+  actualizarSaldoTotal();
+  formGasto.reset();
+});
+
+
 // Editar o eliminar Gastos
 document
   .getElementById("cuerpo-historial-gastos")
@@ -98,39 +285,6 @@ document
     }
   });
 
-// Agregar un nuevo gasto
-botonAgregar.addEventListener("click", (e) => {
-  e.preventDefault();
-  const cantidad = parseFloat(
-    document.getElementById("cantidad-gasto").value
-  );
-  const descripcion = document.getElementById("descripcion-gasto").value;
-  const categoria = document.getElementById("selector-categoria").value;
-  const fecha = new Date().toLocaleDateString();
-
-  if (isNaN(cantidad) || descripcion.trim() === "") {
-    alert("Por favor, completa todos los campos correctamente.");
-    return;
-  }
-
-  if (categoria) {
-    transaccion.registrarGasto(
-      cantidad,
-      descripcion,
-      fecha,
-      categoria
-      
-    );
-  } else {
-    transaccion.registrarGasto(cantidad, descripcion,`sin categoria`, fecha);
-  }
-
-  document.getElementById("categoria-seleccionada").textContent = "";
-  guardarEnLocalStorage("gastos", transaccion.obtenerHistorialGastos());
-  mostrarHistorialGastos();
-  actualizarSaldoTotal();
-  formGasto.reset();
-});
 // Actualizar un gasto existente
 botonActualizar.addEventListener("click", () => {
   const cantidad = parseFloat(document.getElementById("cantidad-gasto").value);
@@ -160,107 +314,6 @@ botonActualizar.addEventListener("click", () => {
   actualizarSaldoTotal();
 });
 
-// Gestión de Ingresos
-// Variables de Referencia para Ingresos
-const formIngreso = document.getElementById("form-ingreso");
-const botonAgregarIngreso = document.getElementById("btn-agregar-ingreso");
-const botonActualizarIngreso = document.getElementById("btn-actualizar-ingreso");
-let indexEnEdicionIngreso = null;
-// Mostrar Historial de Ingresos
-function mostrarHistorialIngresos() {
-  const cuerpoHistorialIngresos = document.getElementById
-  ("cuerpo-historial-ingresos");
-  cuerpoHistorialIngresos.innerHTML = "";
-  const ingresos = transaccion.obtenerHistorialIngresos();
-
-  ingresos.forEach((ingreso, index) => {
-    const fila = document.createElement("tr");
-    fila.innerHTML = `
-      <td>${ingreso.cantidad}</td>
-      <td>${ingreso.descripcion}</td>
-      <td>${ingreso.fecha}</td>
-      <td>
-        <button class="editar-btn-ingreso" data-index="${index}">Editar</button>
-        <button class="eliminar-btn-ingreso" data-index="${index}">Eliminar</button>
-      </td>
-    `;
-    cuerpoHistorialIngresos.appendChild(fila);
-  });
-}
-
-// Editar y Eliminar Ingresos
-document
-  .getElementById("cuerpo-historial-ingresos")
-  .addEventListener("click", (event) => {
-    const boton = event.target;
-
-    // Editar ingreso
-    if (boton.classList.contains("editar-btn-ingreso")) {
-      const index = boton.dataset.index;
-      const ingreso = transaccion.obtenerHistorialIngresos()[index];
-
-      document.getElementById("cantidad-ingreso").value = ingreso.cantidad;
-      document.getElementById("descripcion-ingreso").value = ingreso.descripcion;
-
-      indexEnEdicionIngreso = index;
-      botonAgregarIngreso.style.display = "none";
-      botonActualizarIngreso.style.display = "block";
-    }
-    // Eliminar Ingreso
-    if (boton.classList.contains("eliminar-btn-ingreso")) {
-      const index = boton.dataset.index;
-      transaccion.eliminarIngreso(index);
-      mostrarHistorialIngresos();
-      actualizarSaldoTotal();
-    }
-  });
-// Registrar Ingreso
-botonAgregarIngreso.addEventListener("click", (e) => {
-  e.preventDefault();
-  const cantidad = parseFloat(
-    document.getElementById("cantidad-ingreso").value
-  );
-  const descripcion = document.getElementById("descripcion-ingreso").value;
-  const fecha = new Date().toLocaleDateString();
-
-  if (isNaN(cantidad) || descripcion.trim() === "") {
-    alert("Por favor, completa los campos correctamente.");
-    return;
-  }
-
-  transaccion.registrarIngreso(cantidad, descripcion, fecha);
-  guardarEnLocalStorage("ingresos", transaccion.obtenerHistorialIngresos());
-  mostrarHistorialIngresos();
-  actualizarSaldoTotal();
-  formIngreso.reset();
-});
-
-// Actualizar Ingreso Existente
-botonActualizarIngreso.addEventListener("click", () => {
-  const cantidad = parseFloat(
-    document.getElementById("cantidad-ingreso").value
-  );
-  const descripcion = document.getElementById("descripcion-ingreso").value;
-  const fecha = new Date().toLocaleDateString();
-
-  if (isNaN(cantidad) || descripcion.trim() === "") {
-    alert("Por favor, completa los campos correctamente.");
-    return;
-  }
-  transaccion.editarIngreso(
-    indexEnEdicionIngreso,
-    cantidad,
-    descripcion,
-    fecha
-  );
-  // Restablecer el formulario y botones
-  formIngreso.reset();
-  botonAgregarIngreso.style.display = "block";
-  botonActualizarIngreso.style.display = "none";
-  guardarEnLocalStorage("ingresos", transaccion.obtenerHistorialIngresos());
-  mostrarHistorialIngresos();
-  actualizarSaldoTotal();
-});
 // Guardar un gasto en localStorage
 document.getElementById("form-gasto").addEventListener("submit", (event) => {
   event.preventDefault();
@@ -274,21 +327,29 @@ document.getElementById("form-gasto").addEventListener("submit", (event) => {
   mostrarHistorialGastos();
   actualizarSaldoTotal();
 });
-// Guardar un ingreso en localStorage
-document.getElementById("form-ingreso").addEventListener("submit", (event) => {
-  event.preventDefault();
 
-  const cantidad = parseFloat(
-    document.getElementById("cantidad-ingreso").value
-  );
-  const descripcion = document.getElementById("descripcion-ingreso").value;
-  const fecha = new Date().toLocaleDateString();
 
-  transaccion.registrarIngreso(cantidad, descripcion, fecha);
-  guardarEnLocalStorage("ingresos", transaccion.obtenerHistorialIngresos());
-  mostrarHistorialIngresos();
-  actualizarSaldoTotal();
-});
+// Función para mostrar el historial de gastos
+const mostrarHistorialGastos = () => {
+  const cuerpoHistorial = document.getElementById("cuerpo-historial-gastos");
+  cuerpoHistorial.innerHTML = "";
+  const gastos = transaccion.obtenerHistorialGastos();
+
+  gastos.forEach((gasto, index) => {
+    const fila = document.createElement("tr");
+    fila.innerHTML = `
+      <td>${gasto.cantidad}</td>
+      <td>${gasto.descripcion}</td>
+      <td>${gasto.categoria}</td>
+      <td>${gasto.fecha}</td>
+      <td>
+        <button class="editar-btn" data-index="${index}">Editar</button>
+        <button class="eliminar-btn" data-index="${index}">Eliminar</button>
+      </td>
+    `;
+    cuerpoHistorial.appendChild(fila);
+  });
+}
 
 // Variable para guardar el índice del objetivo que se está editando
 let indiceEdicion = null;
@@ -384,69 +445,6 @@ document.getElementById("lista-objetivos").addEventListener("click", (event) => 
 // Llamar al cargar objetivos iniciales al inicio
 cargarObjetivosIniciales();
 
-
-// Guardar la categoria en localStorage
-document
-  .getElementById("form-categorias")
-  .addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    const nombreCategoria = document.getElementById("nombre-categoria").value;
-
-    if (nombreCategoria.trim() === "") {
-      alert("Por favor, ingresa un nombre válido para la categoría.");
-      return;
-    }
-
-    categorias.crearCategoria(nombreCategoria);
-    const categoriasGuardadas = obtenerDeLocalStorage("categorias") || [];
-    categoriasGuardadas.push(nombreCategoria);
-    guardarEnLocalStorage("categorias", categoriasGuardadas);
-
-    mostrarCategorias();
-
-    document.getElementById("form-categorias").reset();
-  });
-
-document
-  .getElementById("selector-categoria")
-  .addEventListener("change", (event) => {
-    const categoriaSeleccionada = event.target.value;
-    const elementoCategoriaSeleccionada = document.getElementById(
-      "categoria-seleccionada"
-    );
-
-    if (categoriaSeleccionada) {
-      elementoCategoriaSeleccionada.textContent = `Categoría seleccionada: ${categoriaSeleccionada}`;
-    } else {
-      elementoCategoriaSeleccionada.textContent = "";
-    }
-  });
-
-const mostrarCategorias = () => {
-  const categoriasGuardadas = obtenerDeLocalStorage("categorias") || [];
-  const listaCategorias = document.getElementById("lista-categorias");
-  const selectorCategoria = document.getElementById("selector-categoria");
-
-  listaCategorias.innerHTML = "";
-  selectorCategoria.innerHTML =
-    '<option value="">Selecciona una categoría</option>';
-
-  categoriasGuardadas.forEach((categoria) => {
-    const elementoCategoria = document.createElement("li");
-    elementoCategoria.textContent = categoria;
-    listaCategorias.appendChild(elementoCategoria);
-
-    // Añadir categoría al selector
-    const opcion = document.createElement("option");
-    opcion.value = categoria;
-    opcion.textContent = categoria;
-    selectorCategoria.appendChild(opcion);
-  });
-};
-
-document.addEventListener("DOMContentLoaded", mostrarCategorias);
-
 // Función para cargar datos desde `localStorage`
 const cargarDatos = () => {
   const gastosGuardados = obtenerDeLocalStorage("gastos");
@@ -488,4 +486,11 @@ const cargarDatos = () => {
     );
     mostrarCategorias();
   }
+};
+
+// Actualizar saldo total y guardar en localStorage
+const actualizarSaldoTotal = () => {
+  const saldoTotal = transaccion.calcularSaldoTotal();
+  document.getElementById("saldo-total").textContent = `$${saldoTotal}`;
+  guardarEnLocalStorage("saldoTotal", saldoTotal);
 };
