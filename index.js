588591cb7413cd0ac829a5d3879926600e6d1593
Ichurri <santiagoiturrivargas04@gmail.com>
2024-11-25 23:48:35 -0400

    Refactor: crear archivo de barril para reducir lineas de importaciones en el index.js principal

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-import { Usuario } from "./src/clases/Usuario.js";
-import { ObjetivoAhorro } from "./src/clases/ObjetivoAhorro.js";
-import { Transaccion } from "./src/clases/Transaccion.js";
-import { Categorias } from "./src/clases/Categorias.js";
+import { Usuario, ObjetivoAhorro,Transaccion, Categorias } from "./src/clases";
 
 const usuario = Usuario();
 const transaccion = Transaccion();
---
+++ src/clases/index.js
@@ -0,0 +1,4 @@
+export * from './Categorias';
+export * from './ObjetivoAhorro';
+export * from './Transaccion';
+export * from './Usuario';
